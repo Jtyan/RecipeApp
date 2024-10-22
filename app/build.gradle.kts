afcf750cdf48b4b135c5f446512a19a21e2ee898
Joeltt <joelttm15@gmail.com>
2024-10-22 18:49:59 +0100

    feat: add internet permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation ("io.coil-kt:coil:1.4.0")
+    implementation ("io.coil-kt:coil-compose:2.4.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
