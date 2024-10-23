1488bb10986fb93cc1105854d2d969bbcee7bdd9
Joeltt <joelttm15@gmail.com>
2024-10-23 18:27:58 +0100

    feat: Add mealsByCategoryUI page

---
+++ app/src/main/java/learningprogramming/academy/recipeappwithapiandkotlin/MealsByCategoryUI.kt
@@ -0,0 +1,91 @@
+package learningprogramming.academy.recipeappwithapiandkotlin
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun MealsByCategoryUI(category: String) {
+    val recipeViewModel: MainViewModel = viewModel()
+    val viewState by recipeViewModel.mealsState
+
+    recipeViewModel.fetchMealsByCategory(category)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        when {
+            viewState.loading -> {
+                CircularProgressIndicator(Modifier.align(Alignment.Center))
+            }
+
+            viewState.error != null -> {
+                Text("Error Occurred!")
+            }
+
+            else -> {
+                MealCategoryScreen(category, meals = viewState.list)
+            }
+        }
+    }
+}
+
+@Composable
+fun MealCategoryScreen(category: String, meals: List<Meal>) {
+    Column(Modifier.padding(12.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(category, style = MaterialTheme.typography.headlineLarge)
+        Spacer(Modifier.fillMaxWidth().padding(12.dp))
+        LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
+            items(meals, key = { it.idMeal }) { meal ->
+
+                MealsByCategoryItem(meal = meal)
+            }
+        }
+    }
+}
+
+@Composable
+fun MealsByCategoryItem(meal: Meal) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(meal.strMealThumb),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(1f)
+                .clip(CircleShape)
+        )
+        Text(
+            text = meal.strMeal,
+            color = Color.Black,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 4.dp)
+        )
+    }
+}
