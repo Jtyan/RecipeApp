26933143a4dbebeb323525c699e7760618fe56c6
Joeltt <joelttm15@gmail.com>
2024-10-23 18:26:51 +0100

    feat: Add Meal data class

--- app/src/main/java/learningprogramming/academy/recipeappwithapiandkotlin/Meal.kt
+++ app/src/main/java/learningprogramming/academy/recipeappwithapiandkotlin/Meal.kt
@@ -1,2 +1,9 @@
 package learningprogramming.academy.recipeappwithapiandkotlin
 
+data class Meal(
+    val idMeal: String,
+    val strMeal: String,
+    val strMealThumb: String,
+)
+
+data class MealsResponse(val meals: List<Meal>)
